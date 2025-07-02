07fea5d1e620b31c33fc7b7b038ec9a23baa9f83
Marc103 <78170299+Marc103@users.noreply.github.com>
2025-07-02 15:36:57 -0500

    Due to lack of testing infrastructure in Chisel, im swapping over to System Verilog

--- src/test/scala/FloatingPointAdderTest.scala
+++ src/test/scala/FloatingPointAdderTest.scala
@@ -6,6 +6,41 @@ import org.scalatest.flatspec.AnyFlatSpec
  * Test the Floating Point Adder
  */
 
+////////////////////////////////////////////////////////////////
+// Test bench
+/*
+ * "Forked threads provide a concurrency abstraction for writing
+ *  testbenches only, without real parallelism. The test
+ *  infrastructure schedules threads one at a time, with threads
+ *  running once per simulation cycle."
+ *
+ * "Thread order is deterministic, and attempts to follow lexical
+ *  order (as it would appear from the code text): forked (child)
+ *  threads run immediately, then return to the spawning (parent)
+ *  thread. On future cycles, child threads run before their
+ *  parent, in the order they were spawned."
+ *
+ * see https://github.com/ucb-bar/chiseltest
+ *
+ * I tried creating a testbench with a traditional driver, monitor,
+ * and scoreboard using multithreading. The problems are
+ * - forking in this context is just an abstraction
+ * - couldn't find a single multithreaded example that employs the
+ *   regular driver, monitor, scoreboard combo
+ * - it really isn't clear how everything gets synchronized with the
+ *   step of clock
+ * - Although there are thread safe queues, they are not fork safe
+ *   (so what are we supposed to do??)
+ *
+ * Just to make sure, i checked out UC Berkeley's own hard floating
+ * test suite, they use C... I don't think anybody actually uses
+ * chisel test for testing.
+ *
+ * I'm going to swap over to System Verilog. What really sucks is that
+ * chisel does support FV within the toolchain by using the Z3 theorem
+ * solver.
+ */
+
 
 class FloatingPointAdderTest extends AnyFlatSpec  with ChiselScalatestTester {
   "Simple FP32 Manual A + B add test" should "pass" in {
